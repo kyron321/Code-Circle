cbce753af21b9069ccfd6cc434fa368132a3852f
JoeDGit <joe.dobbins89@gmail.com>
2023-01-23 09:54:01 +0000

    Adds initial firebase setup

---
+++ firebase/config.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from 'firebase/app';
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: 'AIzaSyDxBm0urtZBgYT5vP84OsqDzoMqHr0CNtI',
+  authDomain: 'code-circle--x.firebaseapp.com',
+  projectId: 'code-circle--x',
+  storageBucket: 'code-circle--x.appspot.com',
+  messagingSenderId: '250555057230',
+  appId: '1:250555057230:web:b0b6fe6f763bc2f458ef1f',
+  measurementId: 'G-LHZ1H9YKED',
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
