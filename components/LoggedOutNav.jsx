fa17452a583c124c12376ed93ed65b992f40a1db
Kyron Smith <kds060902@outlook.com>
2023-01-24 14:08:42 +0000

    fix:missing import

--- components/LoggedOutNav.jsx
+++ components/LoggedOutNav.jsx
@@ -1,8 +1,9 @@
-import logo from '../images/Logo.svg';
-import arrow from '../images/arrow.svg';
-import Image from 'next/image';
-import styles from '../css/loggedOutNav.module.css';
+import logo from "../images/Logo.svg";
+import arrow from "../images/arrow.svg";
+import Image from "next/image";
+import styles from "../css/loggedOutNav.module.css";
 import React from 'react';
+import { useAuthContext } from "../hooks/useAuthContext";
 export default function LoggedOutNav() {
   //get user from context
   const { user } = useAuthContext();
@@ -14,7 +15,7 @@ export default function LoggedOutNav() {
         <div className={styles.buttonContainerStyle}>
           <button className={styles.loginButtonStyle}>Login</button>
           <button className={styles.tryCodeCircleButtonStyle}>
-            <div className={styles.tryCodeText}>Try Code Circle free</div>{' '}
+            <div className={styles.tryCodeText}>Try Code Circle free</div>{" "}
             <Image src={arrow} alt="button arrow" />
           </button>
         </div>
