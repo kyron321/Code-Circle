c4b7b73f0d6bb7af79efb1519210186097998d68
JoeDGit <joe.dobbins89@gmail.com>
2023-01-24 11:39:42 +0000

    Adds completed logged in nav bar

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -1,10 +1,11 @@
 import Footer from './Footer';
+import LoggedInNav from './LoggedInNav';
 import LoggedOutNav from './LoggedOutNav';
 
 export default function Layout({ children }) {
   return (
     <div>
-      <LoggedOutNav />
+      <LoggedInNav />
       {children}
       <Footer />
     </div>
--- components/LoggedOutNav.jsx
+++ components/LoggedOutNav.jsx
@@ -1,12 +1,9 @@
-import logo from "../images/Logo.svg";
-import arrow from "../images/arrow.svg";
-import Image from "next/image";
-import styles from "../css/loggedOutNav.module.css";
+import logo from '../images/Logo.svg';
+import arrow from '../images/arrow.svg';
+import Image from 'next/image';
+import styles from '../css/loggedOutNav.module.css';
 import React from 'react';
-import { useAuthContext } from "../hooks/useAuthContext";
 export default function LoggedOutNav() {
-  const { user } = useAuthContext();
-  console.log(user)
   return (
     <nav>
       <div className={styles.navContainer}>
@@ -14,7 +11,7 @@ export default function LoggedOutNav() {
         <div className={styles.buttonContainerStyle}>
           <button className={styles.loginButtonStyle}>Login</button>
           <button className={styles.tryCodeCircleButtonStyle}>
-            <div className={styles.tryCodeText}>Try Code Circle free</div>{" "}
+            <div className={styles.tryCodeText}>Try Code Circle free</div>{' '}
             <Image src={arrow} alt="button arrow" />
           </button>
         </div>
