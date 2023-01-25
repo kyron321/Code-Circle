e407883ede31062c7ae792fb407b6b18947206ed
Kyron Smith <kds060902@outlook.com>
2023-01-25 16:01:47 +0000

    chore: comment out socket code causing error

--- utils/messageHandler.jsx
+++ utils/messageHandler.jsx
@@ -1,9 +1,9 @@
 // eslint-disable-next-line import/no-anonymous-default-export
-export default (io, socket) => {
-    const createdMessage = (msg) => {
-      socket.broadcast.emit("newIncomingMessage", msg);
-    };
+// export default (io, socket) => {
+//     const createdMessage = (msg) => {
+//       socket.broadcast.emit("newIncomingMessage", msg);
+//     };
   
-    socket.on("createdMessage", createdMessage);
-  };
+//     socket.on("createdMessage", createdMessage);
+//   };
   
