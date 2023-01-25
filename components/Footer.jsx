313db9ce43980482a0e852488867f9dcfb66fa25
richnw <richardcawdron@hotmail.com>
2023-01-25 15:05:10 +0000

    add footer and change background colour to blue

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -5,8 +5,31 @@ import logo from "../images/Logo.svg";
 
 export default function Footer() {
   return (
-    <main className={styles.main}>
+    <footer className={styles.footer}>
       <Image className={styles.logoStyle} alt="logo" src={logo} />
-    </main>
+      <p> By Team Array of Sunshine</p>
+      <p>
+        <a href="https://github.com/kyron321/Code-Circle">
+          Code Circle on Github
+        </a>
+      </p>
+      <ul>
+        <li className={styles.notLastTeamMember}>
+          <a href="https://github.com/Nasramohammed">Nasramohammed</a>
+        </li>
+        <li className={styles.notLastTeamMember}>
+          <a href="https://github.com/richnw">richnw</a>
+        </li>
+        <li className={styles.notLastTeamMember}>
+          <a href="https://github.com/johnnyfwk">johnnyfwk</a>
+        </li>
+        <li className={styles.notLastTeamMember}>
+          <a href="https://github.com/JoeDGit">JoeDGit</a>
+        </li>
+        <li>
+          <a href="https://github.com/kyron321">kyron321</a>
+        </li>
+      </ul>
+    </footer>
   );
 }
