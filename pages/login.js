b56a0fe244f83c56cd16854c0ff4ce2dbc88abc8
Kyron Smith <kds060902@outlook.com>
2023-01-25 09:29:32 +0000

    chore: remove commented code, make input boxes type password

--- pages/login.js
+++ pages/login.js
@@ -34,7 +34,7 @@ const handleSubmit = async (e) => {
                 <input type="text" id="email" onChange={(e) => {setEmailInput(e.target.value)}}></input>
                 <br />
                 <label htmlFor="password">Enter your password:</label>
-                <input type="text" id="password" onChange={(e) => {setPasswordInput(e.target.value)}}></input>
+                <input type="password" id="password" onChange={(e) => {setPasswordInput(e.target.value)}}></input>
                 <br />
                 <button>Submit</button>
             </form>
