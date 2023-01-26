299f8df968610a07ae6c09066b0bf9027ff3738a
JoeDGit <joe.dobbins89@gmail.com>
2023-01-26 15:52:32 +0000

    Adds working user profile & other user profile

--- components/UserProfile.jsx
+++ components/UserProfile.jsx
@@ -1,10 +1,67 @@
-import React from "react";
-import styles from "../css/userProfile.module.css";
+import React, { useEffect, useState } from 'react';
+import styles from '../css/userProfile.module.css';
+import { collection, query, where, getDocs } from 'firebase/firestore';
+import { db } from '../firebase/config';
+import Image from 'next/image';
+import defaultAvatar from '../images/default-avatar.svg';
+import { TbMessage2 } from 'react-icons/tb';
+import { AiOutlinePhone } from 'react-icons/ai';
 
-export default function UserProfile() {
+export default function UserProfile({ userName, userNameFromParams }) {
+  const [user, setUser] = useState({});
+  const [isLoading, setIsLoading] = useState(true);
+
+  const isSomeoneElsesProfile = userName !== userNameFromParams;
+
+  useEffect(() => {
+    setIsLoading(true);
+    getUser()
+      .then((response) => {
+        setUser(response[0]);
+        setIsLoading(false);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, []);
+
+  async function getUser() {
+    const usersCol = collection(db, 'users');
+    const userQuery = query(
+      usersCol,
+      where('displayname', '==', `${userNameFromParams}`)
+    );
+    const usersSnapshot = await getDocs(userQuery);
+    const usersList = usersSnapshot.docs.map((doc) => doc.data());
+    return usersList;
+  }
+
+  if (isLoading) return <div>Loading...</div>;
   return (
     <main className={styles.profile}>
-      <h1 className={styles.displayName}>DisplayName</h1>
+      <div className={styles.avatarAndNameContainer}>
+        <Image
+          className={styles.avatarStyle}
+          alt="user Avatar"
+          src={user?.avatar ? user?.avatar : defaultAvatar}
+        />
+
+        <h2 className={styles.displayName}>{userNameFromParams}</h2>
+        {isSomeoneElsesProfile ? (
+          <div className={styles.contactOtherUser}>
+            <TbMessage2 size={30} style={{ color: 'white' }} />
+            <AiOutlinePhone size={30} style={{ color: 'white' }} />
+          </div>
+        ) : null}
+      </div>
+      <div className={styles.techStack}>
+        <h3>TechStack</h3>
+        <div className={styles.techStack}>
+          {user?.techstack?.map((tech) => {
+            return <div key={tech}>{tech}</div>;
+          })}
+        </div>
+      </div>
     </main>
   );
 }
--- pages/users/[displayname].js
+++ pages/users/[displayname].js
@@ -1,9 +1,21 @@
-import React from "react";
-import styles from "../css/otherUserProfile.module.css";
-import { useRouter } from "next/router";
-import { useAuthContext } from "../../hooks/useAuthContext";
-import UserProfile from "../../components/UserProfile";
+import React, { useState } from 'react';
+import styles from '../../css/userProfile.module.css';
+import { useRouter } from 'next/router';
+import UserProfile from '../../components/UserProfile';
+import { useAuthContext } from '../../hooks/useAuthContext';
 
 export default function OtherUserProfile() {
-  return <UserProfile />;
+  const { user } = useAuthContext();
+  const [isUsersOwnProfile, setIsUsersOwnProfile] = useState(false);
+  let { query } = useRouter();
+  let userNameFromParams = query.displayname;
+
+  return (
+    <div className={styles.pageContainer}>
+      <UserProfile
+        userName={user?.displayName}
+        userNameFromParams={userNameFromParams}
+      />
+    </div>
+  );
 }
