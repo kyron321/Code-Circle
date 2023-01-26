d2cfa052a8cec9be0d32a16825cff5f1e0bacb38
JoeDGit <joe.dobbins89@gmail.com>
2023-01-26 11:09:14 +0000

    adds: timestamp to each new user post

--- hooks/useCreateAPost.jsx
+++ hooks/useCreateAPost.jsx
@@ -1,25 +1,32 @@
 import { db } from '../firebase/config';
-import { addDoc, collection } from "firebase/firestore";
-import { doc, updateDoc } from "firebase/firestore";
+import { addDoc, collection } from 'firebase/firestore';
+import { doc, updateDoc } from 'firebase/firestore';
 
-export const createAPost = (postTitle, projectDescription, programmingLanguage, timeToCode, timeZone) => {
-    try {
-        const docRef = addDoc(collection(db, "posts"), {
-            postTitle: postTitle,
-            projectDescription: projectDescription,
-            programmingLanguage: programmingLanguage,
-            timeToCode: timeToCode,
-            timeZone: timeZone
-        })
-        .then((response) => {
-            console.log(response.id);
-            const post = doc(db, "posts", response.id);
-            updateDoc(post, {
-                postId: response.id
-            });
-        })
-        console.log("Document written with ID: ", docRef.id);
-    } catch(e) {
-        console.error("Error adding document: ", e);
-    }
-}
+export const createAPost = (
+  postTitle,
+  projectDescription,
+  programmingLanguage,
+  timeToCode,
+  timeZone
+) => {
+  const newDate = new Date();
+  try {
+    const docRef = addDoc(collection(db, 'posts'), {
+      postTitle: postTitle,
+      projectDescription: projectDescription,
+      programmingLanguage: programmingLanguage,
+      timeToCode: timeToCode,
+      timeZone: timeZone,
+      postTime: newDate.getTime(),
+    }).then((response) => {
+      console.log(response.id);
+      const post = doc(db, 'posts', response.id);
+      updateDoc(post, {
+        postId: response.id,
+      });
+    });
+    console.log('Document written with ID: ', docRef.id);
+  } catch (e) {
+    console.error('Error adding document: ', e);
+  }
+};
