b68a0ab810764eab7a2c13bb92343e09bd418152
richnw <richardcawdron@hotmail.com>
2023-01-27 13:55:13 +0000

    add temporary firebase config

--- firebase/config.js
+++ firebase/config.js
@@ -1,20 +1,24 @@
 // Import the functions you need from the SDKs you need
-import { initializeApp } from 'firebase/app';
-import { getAuth } from 'firebase/auth';
-import { getFirestore } from 'firebase/firestore';
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: 'AIzaSyDxBm0urtZBgYT5vP84OsqDzoMqHr0CNtI',
-  authDomain: 'code-circle--x.firebaseapp.com',
-  projectId: 'code-circle--x',
-  storageBucket: 'code-circle--x.appspot.com',
-  messagingSenderId: '250555057230',
-  appId: '1:250555057230:web:b0b6fe6f763bc2f458ef1f',
-  measurementId: 'G-LHZ1H9YKED',
+  apiKey: "AIzaSyA1ootVgJ89u4TYCjXs4FwbvRL2U4H_eQs",
+
+  authDomain: "rich-branch-41bc2.firebaseapp.com",
+
+  projectId: "rich-branch-41bc2",
+
+  storageBucket: "rich-branch-41bc2.appspot.com",
+
+  messagingSenderId: "3217860387",
+
+  appId: "1:3217860387:web:32d1e9f639ec1f7d54ce96",
 };
 
 // Initialize Firebase
