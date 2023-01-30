59938f9abc153438c0c4a2d0d337c51059f9faad
JoeDGit <joe.dobbins89@gmail.com>
2023-01-30 11:28:49 +0000

    Stashes morning progress

--- pages/screen-sharing.js
+++ pages/screen-sharing.js
@@ -1,58 +1,60 @@
 import { useState, useRef } from 'react';
 
 export default function ScreenSharing() {
+  const vidRef = useRef(null);
 
-    const vidRef = useRef(null);
+  const gdmOptions = {
+    video: true,
+    audio: true,
+  };
 
-    const gdmOptions = {
-        video: true,
-        audio: true
-    }
-
+  function startCapture() {
     const displayMediaOptions = {
-        video: {
-          displaySurface: "window"
-        },
-        audio: false
+      video: {
+        displaySurface: 'window',
+      },
+      audio: false,
     };
+    navigator.mediaDevices
+      .getDisplayMedia(displayMediaOptions)
+      .then((mediaStream) => {
+        console.log(mediaStream);
+        vidRef.current.srcObject = mediaStream;
+      })
+      .catch((err) => {
+        console.error(`Error:${err}`);
+        return null;
+      });
+  }
 
-    function startCapture(displayMediaOptions) {
-        navigator.mediaDevices.getDisplayMedia(displayMediaOptions)
-            .then((mediaStream) => {
-                console.log(mediaStream);
-                vidRef.current.srcObject = mediaStream;
-            })
-            .catch((err) => { console.error(`Error:${err}`); return null; });
-    }
+  function stopCapture() {
+    let tracks = vidRef.current.srcObject.getTracks();
+    tracks.forEach((track) => track.stop());
+    vidRef.current.srcObject = null;
+  }
 
-    function stopCapture() {
-        let tracks = vidRef.current.srcObject.getTracks();
-        tracks.forEach((track) => track.stop());
-        vidRef.current.srcObject = null;
-    }
+  return (
+    <main>
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <h1>Screen Sharing</h1>
 
-    return (
-        <main>
-            <br />
-            <br />
-            <br />
-            <br />
-            <br />
-            <br />
-            <h1>Screen Sharing</h1>
+      <button onClick={startCapture}>Start Screen Capture</button>
+      <button onClick={stopCapture}>Stop Screen Capture</button>
 
-            <button onClick={startCapture}>Start Screen Capture</button>
-            <button onClick={stopCapture}>Stop Screen Capture</button>
+      <br />
 
-            <br />
+      <video style={{ width: '100%' }} ref={vidRef} autoPlay></video>
 
-            <video style={{width: '100%'}} ref={vidRef} autoPlay></video>
+      <br />
 
-            <br />
-
-            <strong>Log:</strong>
-            <br />
-            <pre id="log"></pre>
-        </main>
-    )
+      <strong>Log:</strong>
+      <br />
+      <pre id="log"></pre>
+    </main>
+  );
 }
