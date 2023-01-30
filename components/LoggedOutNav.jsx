635d80c858bfbf1b33158f5c26f39510e051dc64
Kyron Smith <kds060902@outlook.com>
2023-01-30 13:37:32 +0000

    feat: create reusable modal - used for responsive navbars

--- components/LoggedOutNav.jsx
+++ components/LoggedOutNav.jsx
@@ -2,14 +2,38 @@ import logo from "../images/Logo.svg";
 import arrow from "../images/arrow.svg";
 import Image from "next/image";
 import styles from "../css/loggedOutNav.module.css";
-import React from "react";
+import React, { useState } from "react";
 import { useRouter } from "next/router";
 import Link from "next/link";
 import Button from "./Button";
 import { GiHamburgerMenu } from "react-icons/gi";
+import Modal from "./Modal";
 
 export default function LoggedOutNav() {
   const router = useRouter();
+  const [showModal, setShowModal] = useState(false);
+
+  const modal = (
+    <Modal setShowModal={setShowModal} showModal={showModal}>
+      <Button
+        label="Login"
+        type="primary"
+        size="medium"
+        onClick={() => {
+          router.push("/login");
+        }}
+      />
+      <Button
+        label="Try Code Circle free"
+        type="secondary"
+        size="medium"
+        image={arrow}
+        onClick={() => {
+          router.push("/create-an-account");
+        }}
+      />
+    </Modal>
+  );
 
   return (
     <nav>
@@ -17,7 +41,10 @@ export default function LoggedOutNav() {
         <Link href="/">
           <Image className={styles.logoStyle} alt="logo" src={logo} />
         </Link>
-        <GiHamburgerMenu className={styles.hamburgerMenuStyle} />
+        {showModal && modal}
+        <GiHamburgerMenu onClick={()=>{
+          setShowModal(!showModal)
+        }} className={styles.hamburgerMenuStyle} />
         <div className={styles.buttonContainerStyle}>
           <Button
             label="Login"
