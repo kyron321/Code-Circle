f9688227dad288ee8b398583321dde610a352f37
Kyron Smith <kds060902@outlook.com>
2023-01-31 22:28:12 +0000

    feat: add error handling functionality for signup page

--- components/LoaderButton.jsx
+++ components/LoaderButton.jsx
@@ -2,7 +2,7 @@ import styles from '../css/loaderButton.module.css'
 export default function Loader() {
   return (
     <div className={styles.container}>
-        <div className={lds-ring}><div>Hello</div><div></div><div></div><div></div></div>
+        <div className={styles.ring}><div></div><div></div><div></div><div></div></div>
     </div>
     )
 }
