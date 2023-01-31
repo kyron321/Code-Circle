8b35522bb4a2a8ef6ac07b60952587013e25b6a3
richnw <richardcawdron@hotmail.com>
2023-01-31 14:49:55 +0000

    add home page not logged in redirect

--- pages/home.js
+++ pages/home.js
@@ -1,5 +1,31 @@
 import Posts from "../components/Posts";
+import { useAuthContext } from "../hooks/useAuthContext";
+import { Router, useRouter } from "next/router";
+import { useContext, useEffect } from "react";
 
 export default function Home() {
+  const router = useRouter();
+  const user = Promise.resolve(useAuthContext());
+
+  user.then((user) => {
+    if (!user.user && user.authIsReady) {
+      router.push("/login");
+    }
+  });
+
   return <Posts />;
+
+  // useEffect(() => {
+  //   getCurrentUser()
+  //   if (!user) router.push("/");
+  // }, []);
+
+  // return <Posts />;
+
+  // useEffect(() => {
+  //   console.log(user);
+  //   if (!user) {
+  //     router.push("/");
+  //   }
+  // }, []);
 }
