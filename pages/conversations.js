bb634a2bf200ad6303d2e2c2965fe49d1d0fd6b6
JoeDGit <joe.dobbins89@gmail.com>
2023-02-01 10:13:39 +0000

    Adds page to handle no current conversations active

--- pages/conversations.js
+++ pages/conversations.js
@@ -1,12 +1,13 @@
-import React, { useEffect, useState } from "react";
-import { useRouter } from "next/router";
+import React, { useEffect, useState } from 'react';
+import { useRouter } from 'next/router';
 
-import { getConversations } from "../hooks/getConversations";
-import { useAuthContext } from "../hooks/useAuthContext";
+import { getConversations } from '../hooks/getConversations';
+import { useAuthContext } from '../hooks/useAuthContext';
 
-import ConversationCard from "../components/ConversationCard";
+import ConversationCard from '../components/ConversationCard';
+import EmptyConvoPage from '../components/EmptyConvoPage';
 
-import styles from "../css/conversations.module.css";
+import styles from '../css/conversations.module.css';
 
 export default function Conversations() {
   const [conversations, setConversations] = useState([]);
@@ -44,33 +45,31 @@ export default function Conversations() {
   });
 
   if (isLoading) return <div>Loading...</div>;
+  if (filteredConversations?.length < 1) return <EmptyConvoPage />;
+  return (
+    <div>
+      <h1 className={styles.heading}>Conversations</h1>
 
-  if (filteredConversations?.length > 0) {
-    return (
-      <div>
-        <h1 className={styles.heading}>Conversations</h1>
-
-        <section className={styles.conversations}>
-          {Object.keys(grouped).map((item, i) => (
-            <div
+      <section className={styles.conversations}>
+        {Object.keys(grouped).map((item, i) => (
+          <div
             key={i}
-              onClick={() =>
-                handleRedirect(
-                  grouped?.[item]?.[0]?.channel,
-                  grouped?.[item]?.[0]?.user
-                )
-              }
-            >
-              <ConversationCard
-                key={`${item}-${i}`}
-                user={grouped?.[item]?.[0]?.user}
-                date={grouped?.[item]?.[0]?.createdAt?.toLocaleString()}
-                message={grouped?.[item]?.[0]?.message}
-              />
-            </div>
-          ))}
-        </section>
-      </div>
-    );
-  }
+            onClick={() =>
+              handleRedirect(
+                grouped?.[item]?.[0]?.channel,
+                grouped?.[item]?.[0]?.user
+              )
+            }
+          >
+            <ConversationCard
+              key={`${item}-${i}`}
+              user={grouped?.[item]?.[0]?.user}
+              date={grouped?.[item]?.[0]?.createdAt?.toLocaleString()}
+              message={grouped?.[item]?.[0]?.message}
+            />
+          </div>
+        ))}
+      </section>
+    </div>
+  );
 }
