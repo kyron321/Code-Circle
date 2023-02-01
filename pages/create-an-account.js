86c0ba700a3b4984f8f059f4665af56cbe5130c5
Kyron Smith <kds060902@outlook.com>
2023-02-01 15:58:47 +0000

    fix: made button dynamic in side bar, inital null value

--- pages/create-an-account.js
+++ pages/create-an-account.js
@@ -1,6 +1,8 @@
 import SignupForm from "../components/SignupForm";
 import FormSide from "../components/FormSide";
 import styles from "../css/login.module.css";
+import Button from "../components/Button";
+import arrow from "../images/arrow.svg";
 
 export default function CreateAnAccount() {
   return (
@@ -8,7 +10,17 @@ export default function CreateAnAccount() {
       <FormSide
         title="Welcome to Code Circle 
         Join Us "
-        descritpion="Empowering developers to come together and create, our platform is your ultimate collaboration partner for coding success."
+        description="Empowering developers to come together and create, our platform is your ultimate collaboration partner for coding success."
+        button={
+          <Button
+            label={"Find Out More"}
+            onClick={() => {
+              router.push("/");
+            }}
+            image={arrow}
+            className={styles.side_btn}
+          />
+        }
       />
       <SignupForm />;
     </div>
